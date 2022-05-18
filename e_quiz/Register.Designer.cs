6e97b6c52474c62974dd4b881d22ffaa78ab2dfa
MislinaSarac <75904703+MislinaSarac@users.noreply.github.com>
2022-05-18 17:08:31 +0300

    feat: regiter page changed

--- e_quiz/Register.Designer.cs
+++ e_quiz/Register.Designer.cs
@@ -30,9 +30,6 @@ namespace e_quiz
         private void InitializeComponent()
         {
             this.btnLogin = new System.Windows.Forms.Button();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.txtConfrimPassword = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
@@ -44,7 +41,7 @@ namespace e_quiz
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(258, 538);
+            this.btnLogin.Location = new System.Drawing.Point(253, 469);
             this.btnLogin.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(267, 63);
@@ -52,45 +49,6 @@ namespace e_quiz
             this.btnLogin.Text = "Register";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Forte", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.radioButton3.Location = new System.Drawing.Point(322, 461);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(125, 29);
-            this.radioButton3.TabIndex = 20;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Examiner";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Forte", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.radioButton2.Location = new System.Drawing.Point(521, 461);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(108, 29);
-            this.radioButton2.TabIndex = 19;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Student";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Forte", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.radioButton1.Location = new System.Drawing.Point(151, 461);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(99, 29);
-            this.radioButton1.TabIndex = 18;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Admin";
-            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // txtConfrimPassword
             // 
@@ -168,11 +126,8 @@ namespace e_quiz
             this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGreen;
-            this.ClientSize = new System.Drawing.Size(739, 685);
+            this.ClientSize = new System.Drawing.Size(757, 590);
             this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.txtConfrimPassword);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtPassword);
@@ -193,9 +148,6 @@ namespace e_quiz
         #endregion
 
         private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.TextBox txtConfrimPassword;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtPassword;
